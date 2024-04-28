065fa26ab73289049dfbe2800f5a5e6791b909d9
Mukul <mukulpraj9@gmail.com>
2024-04-29 04:10:12 +0530

    adding readme file

---
+++ src/main/java/com/designs/lld/LowLevelDesignsApplication.java
@@ -0,0 +1,13 @@
+package com.designs.lld;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class LowLevelDesignsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(LowLevelDesignsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/designs/lld/LowLevelDesignsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.designs.lld;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class LowLevelDesignsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
