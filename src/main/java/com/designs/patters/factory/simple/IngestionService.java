d587dce23a00bc70e2cd3ec4ae7da28fba9688f7
Mukul <mukulpraj9@gmail.com>
2024-05-13 10:37:21 +0530

    code refactor

--- src/main/java/com/designs/patters/factory/simple/IngestionService.java
+++ src/main/java/com/designs/patters/factory/simple/IngestionService.java
@@ -2,14 +2,4 @@ package com.designs.patters.factory.simple;
 
 public class IngestionService {
 
-    FactoryOfSource source = new FactoryOfSource();
-
-    public IngestionService(FactoryOfSource source_) {
-        this.source = source_;
-    }
-
-    public void ingestData(String sourceName) {
-        IngestToDataBase sourceObject = source.getInstanceOfSource(sourceName);
-        sourceObject.ingestData();
-    }
 }
--- src/main/java/com/designs/patters/factory/simple/Main.java
+++ src/main/java/com/designs/patters/factory/simple/Main.java
@@ -3,10 +3,10 @@ package com.designs.patters.factory.simple;
 public class Main {
     public static void main(String[] args) {
 
-        FactoryOfSource source = new FactoryOfSource();
-        IngestionService service = new IngestionService(source);
-        service.ingestData("api");
-        service.ingestData("dataLake");
-        service.ingestData("file");
+//        FactoryOfSource source = new FactoryOfSource();
+//        IngestionService service = new IngestionService(source);
+//        service.ingestData("api");
+//        service.ingestData("dataLake");
+//        service.ingestData("file");
     }
 }
